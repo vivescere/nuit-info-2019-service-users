23cdb32074b1bf1ab15835d3cb77ccd586d67c5d
Nicolas Bourras <vivescere@gmail.com>
2019-12-04 22:40:33 +0100

    Trying out migrations

--- server.py
+++ server.py
@@ -1,6 +1,14 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:@postgres'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+
+  id = db.Column(db.Integer, primary_key=True)
+  email = db.Column(db.String(128), unique=True, nullable=False)
 
 @app.route('/')
 def index():
@@ -10,5 +18,9 @@ def index():
 def get_users():
   return jsonify({'test': True})
 
-if __name__ == '__main__':
+def start_app():
+  db.create_all()
   app.run(port=3000)
+
+if __name__ == '__main__':
+  start_app()
